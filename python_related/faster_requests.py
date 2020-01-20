4dcb181554f67ce02472e2aea8c5aef311c09d62
tusonggao <tusonggao@163.com>
2020-01-21 07:48:14 +0800

    update files

--- python_related/faster_requests.py
+++ python_related/faster_requests.py
@@ -9,7 +9,18 @@ for _ in range(100):
     resp = requests.get('https://baidu.com').content.decode()
 end_t = time.time()
 
-print(f'访问一百次百度网页，耗时:{end_t-start_t}')
+print(f'不使用Session 访问一百次百度网页，耗时:{end_t-start_t}')
 
 ###################################################################################
 
+start_t = time.time()
+session = requests.Session()
+for _ in range(100):
+    resp = session.get('https://baidu.com').content.decode()
+end_t = time.time()
+
+print(f'使用Session 访问一百次百度网页，耗时:{end_t-start_t}')
+
+# 运行结果，输出如下：
+# 不使用Session 访问一百次百度网页，耗时:22.324612855911255
+# 使用Session 访问一百次百度网页，耗时:5.712132453918457
